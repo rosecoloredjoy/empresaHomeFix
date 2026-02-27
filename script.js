e1855a16b8cd852f2d30532ac05919c51101d49a
rosecoloredjoy <nayaguima@gmail.com>
2026-02-26 22:36:51 -0300

    idk

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("it worked!!");
+
+let nome = "Nayara";
+let idade = 20;
+
+
+function saudacao() {
+    console.log("Olá, " + nome + "! Confira nossos produtos.");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Chamar função
+    saudacao();
+
+     });
